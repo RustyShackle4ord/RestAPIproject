bf6abbc410a49ed01c59ab59a8afc7762f21f4cc
RustyShackle4ord <127082244+RustyShackle4ord@users.noreply.github.com>
2023-10-06 17:45:06 -0600

    Add files via upload

---
+++ RESTAPI_Project/cmd/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "github.com/gofiber/fiber/v2"
+    "github.com/divrhino/divrhino-trivia/database"
+)
+
+func main() {
+    database.ConnectDb()
+    app := fiber.New()
+
+    setupRoutes(app)
+
+
+
+    app.Listen(":3000")
+}
---
+++ RESTAPI_Project/database/database.go
@@ -0,0 +1,45 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/divrhino/divrhino-trivia/models"
+	"gorm.io/gorm"
+	"gorm.io/driver/postgres"
+)
+
+type Dbinstance struct {
+	Db *gorm.DB
+}
+
+var DB Dbinstance
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=db user=%s password=%s dbname=%s port=5432 sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+	
+	log.Println("Connected")
+	db.Logger = logger.Default.LogMode(logger.info)
+
+	log.Println("running migrations")
+	db.AutoMigrate(&models.Fact{})
+
+	DB = Dbinstance{
+		Db: db,
+	}
+}
